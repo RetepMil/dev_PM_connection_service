0b90b61fc6a423800cafeaf9aa37ffdea698afb9
hyeongwoo.lim <lkslyj2@naver.com>
2022-01-20 17:48:58 +0900

    add: project logo in navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -47,6 +47,15 @@ const Navigation = () => {
         >
           <span className={styles.find_engineer}>find engineer</span>
         </div>
+        <div className={styles.logo_container}>
+          <img
+            height="50px"
+            width="50px"
+            alt="nav_puzzle_logo"
+            src={process.env.PUBLIC_URL + "/img/Navigation/nav_puzzle_logo.png"}
+          />
+          <span>project</span>
+        </div>
       </div>
     </>
   );
